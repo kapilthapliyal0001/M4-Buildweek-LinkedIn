dde6d97c872e68d70ed41abba29a63162dc5ea1c
Helena Nel <mailhedri@gmail.com>
2021-06-14 16:17:43 +0200

    Update ProfileAbout.jsx

---
+++ src/components/Profile/ProfileAbout.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Card } from "react-bootstrap";
+import { Pencil } from "react-bootstrap-icons";
+
+import "./JumboProfile.css";
+export default class ProfileAbout extends Component {
+  render() {
+    return (
+      <>
+        <Card className="my-2" id="aboutProfile">
+          <Card.Title id="aboutProfile_title" className="mt-1">
+            <span>{this.props.title}</span>
+            <Pencil />
+          </Card.Title>
+          <Card.Body id="aboutProfile_body">
+            <Card.Text>
+              <p>{this.props.bio}</p>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </>
+    );
+  }
+}
