350e1742bf656ddbc59a545287213506f7906abc
Helena Nel <mailhedri@gmail.com>
2021-06-14 16:26:43 +0200

    commit

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -10,8 +10,8 @@ export default class ProfilePage extends Component {
   };
 
   componentDidMount = async () => {
-    // const userId = "60c73bf1291930001560aba3";
-    const endpointGetMyProfile = `https://striveschool-api.herokuapp.com/api/profile/`;
+    const userId = "60c73bf1291930001560aba3";
+    const endpointGetMyProfile = `https://striveschool-api.herokuapp.com/api/profile/${userId}}`;
     const bearerTokenHedri =
       "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGM3M2JmMTI5MTkzMDAwMTU2MGFiYTMiLCJpYXQiOjE2MjM2Njk3NDUsImV4cCI6MTYyNDg3OTM0NX0.Lk5Z-l56SBkY6YCIvoiHpVg_0J0rEZHaO4PzAuep3bo";
 
