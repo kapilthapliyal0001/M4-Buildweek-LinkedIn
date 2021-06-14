847abe1d74daad85608653dbbc4665f94f57b6d3
Kapil Thapliyal <kapilthapliyal0001@gmail.com>
2021-06-14 15:08:54 +0200

    styling needed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Sidebar from "./components/Sidebar/Sidebar";
+import MainPage from "./components/Sidebar/MainPage"; // main page is not useed right now;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Sidebar />
     </div>
   );
 }
