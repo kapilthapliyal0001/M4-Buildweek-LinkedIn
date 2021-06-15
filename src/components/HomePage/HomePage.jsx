6a315f1869374e76caabb7aaba6f7eaebd2e1a23
Helena Nel <mailhedri@gmail.com>
2021-06-15 02:22:45 +0200

    added Home structure

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Feed from "./Feed/Feed";
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <>
+        <Feed />
+      </>
+    );
+  }
+}
