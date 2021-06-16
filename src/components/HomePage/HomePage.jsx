f5ae02ae01272597479330da3ea876950b5b2f11
Helena Nel <mailhedri@gmail.com>
2021-06-16 09:57:45 +0200

    moved fetch call to HomePage

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -6,16 +6,43 @@ import SidebarLeftMain from "./SidebarLeft/SidebarLeftMain";
 import "./SidebarLeft/SidebarLeft.css";
 
 export default class HomePage extends Component {
+  state = {
+    user: {},
+  };
+
+  componentDidMount = async () => {
+    try {
+      const bearerTokenHedri =
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGM3M2JmMTI5MTkzMDAwMTU2MGFiYTMiLCJpYXQiOjE2MjM2Njk3NDUsImV4cCI6MTYyNDg3OTM0NX0.Lk5Z-l56SBkY6YCIvoiHpVg_0J0rEZHaO4PzAuep3bo";
+
+      const response = await fetch(
+        `https://striveschool-api.herokuapp.com/api/profile/me`,
+        {
+          headers: {
+            Authorization: bearerTokenHedri,
+          },
+        }
+      );
+      if (response.ok) {
+        const data = await response.json();
+        console.log("This is sidebar profile data", data);
+        this.setState({ user: data });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   render() {
+    const { user } = this.state;
     return (
       <>
         <Container>
           <Row>
             <Col xs={3}>
-              <SidebarLeftMain />
+              <SidebarLeftMain user={user} />
             </Col>
             <Col xs={6}>
-              {" "}
               <MainFeed />
             </Col>
             <Col xs={3}></Col>
--- src/components/HomePage/SidebarLeft/SidebarLeftMain.jsx
+++ src/components/HomePage/SidebarLeft/SidebarLeftMain.jsx
@@ -4,43 +4,15 @@ import SidebarLeftColOne from "./SidebarLeftColOne";
 import SidebarLeftColTwo from "./SidebarLeftColTwo";
 
 class SidebarLeftMain extends React.Component {
-  state = {
-    user: {},
-    userExperiences: [],
-  };
-
-  componentDidMount = async () => {
-    try {
-      const bearerTokenHedri =
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGM3M2JmMTI5MTkzMDAwMTU2MGFiYTMiLCJpYXQiOjE2MjM2Njk3NDUsImV4cCI6MTYyNDg3OTM0NX0.Lk5Z-l56SBkY6YCIvoiHpVg_0J0rEZHaO4PzAuep3bo";
-
-      const response = await fetch(
-        `https://striveschool-api.herokuapp.com/api/profile/me`,
-        {
-          headers: {
-            Authorization: bearerTokenHedri,
-          },
-        }
-      );
-      if (response.ok) {
-        const data = await response.json();
-        console.log("This is sidebar profile data", data);
-        this.setState({ user: data });
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   render() {
     return (
       <>
         <SidebarLeftColOne
-          name={this.state.user.name}
-          surname={this.state.user.surname}
-          title={this.state.user.title}
-          area={this.state.user.area}
-          image={this.state.user.image}
+          name={this.props.user.name}
+          surname={this.props.user.surname}
+          title={this.props.user.title}
+          area={this.props.user.area}
+          image={this.props.user.image}
         />
         <SidebarLeftColTwo />
       </>
